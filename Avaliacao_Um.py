6d3348bb44fd43f750e83f770c14446a9c59cc2d
Akijunior <suitsu19@gmail.com>
2017-11-09 16:01:04 -0200

    Versão atualizada.

--- Avaliacao_Um.py
+++ Avaliacao_Um.py
@@ -2,22 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 
 def search(keyword, url, deth):
-
-    # url = 'https://example.webscraping.com/places/view/United-Kingdom-239'
-    # html = download(url)
-    # soup = BeautifulSoup(html, 'html.parser')
-    # tr = soup.find(attrs={"id": "places_area__row"})
-    # td = tr.find(attrs={'class': 'w2p_fw'})
-    # area = td.text
-    # print(area)
+    
     response = requests.get(url)
     html_com_tags = response.text
     html_sem_tags = BeautifulSoup(html_com_tags, 'html.parser')
     contexto = ""
+
     for i in range(len(html_sem_tags.text)):
-        if (html_sem_tags.text[i:i+len(keyword)] == keyword):
-            contexto = html_sem_tags.text[i-10:i+len(keyword)+11]
-    print (contexto + " " + url)
+        if (html_sem_tags.text[i:i + len(keyword)] == keyword):
+            contexto = html_sem_tags.text[i - 10:i + len(keyword) + 11]
+            while deth > 0:
+                search(keyword, url, deth - 1)
+
+    print(contexto + " " + url)
 
 search('animais vertebrados', 'https://www.google.com/search?q=peixe', 0)
 
